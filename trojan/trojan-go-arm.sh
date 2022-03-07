23e3bd9f305e9d06775d5862be466d4b4c8c7195
ssfun <jiegto@outlook.com>
2022-03-07 14:50:10 +0800

    Update trojan-go-arm.sh

--- trojan/trojan-go-arm.sh
+++ trojan/trojan-go-arm.sh
@@ -3,23 +3,22 @@
 echo "Getting the latest version of trojan-go"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
 echo "${latest_version}"
-trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/${latest_version}/trojan-go-linux-arm.zip"
+trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/${latest_version}/trojan-go-linux-armv8.zip"
 
-mkdir -p "/usr/bin/trojan-go"
 mkdir -p "/etc/trojan-go"
 
 cd `mktemp -d`
 wget -nv "${trojango_link}" -O trojan-go.zip
 unzip -q trojan-go.zip && rm -rf trojan-go.zip
 
-mv trojan-go /usr/bin/trojan-go/trojan-go
+mv trojan-go /usr/bin/trojan-go
 mv geoip.dat /etc/trojan-go/geoip.dat
 mv geosite.dat /etc/trojan-go/geosite.dat
 mv example/trojan-go.service /etc/systemd/system/trojan-go.service
 
 # if config.json didn't exist, use the example server.json 
 if [ ! -f "/etc/trojan-go/config.json" ]; then
-  mv /example/sever.json /etc/trojan-go/config.json
+  mv example/server.json /etc/trojan-go/config.json
 fi
 
 systemctl daemon-reload
