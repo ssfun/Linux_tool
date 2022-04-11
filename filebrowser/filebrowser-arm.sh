61654a2b2ebc6f0ee65e337815593ae89da1eaf2
sfun <jiegto@outlook.com>
2022-04-11 08:01:51 +0800

    Update filebrowser-arm.sh

--- filebrowser/filebrowser-arm.sh
+++ filebrowser/filebrowser-arm.sh
@@ -6,24 +6,45 @@ echo "${latest_version}"
 filebrowser_link="https://github.com/filebrowser/filebrowser/releases/download/${latest_version}/linux-arm64-filebrowser.tar.gz"
 
 mkdir -p "/etc/filebrowser"
+mkdir -p "/var/log/filebrowser"
 mkdir -p "/opt/filebrowser"
 mkdir -p "/home/filebrowser"
 
 cd `mktemp -d`
 wget -nv "${filebrowser_link}" -O filebrowser.tar.gz
 tar -zxvf filebrowser.tar.gz  && rm -rf filebrowser.tar.gz && rm -rf CHANGELOG.md && rm -rf README.md && rm -rf LICENSE
+mv filebrowser /usr/local/bin/filebrowser && chmod +x /usr/local/bin/filebrowser
 
-wget https://raw.githubusercontent.com/ssfun/Linux_tool/main/filebrowser/filebrowser.service
-wget https://raw.githubusercontent.com/ssfun/Linux_tool/main/filebrowser/config.json
+# set filebrowser.service
+cat <<EOF >/etc/systemd/system/filebrowser.service
+[Unit]
+Description=filebrowser
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
 
+[Service]
+User=root
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/filebrowser -c /etc/filebrowser/config.json
 
-mv filebrowser /usr/local/bin/filebrowser
-mv filebrowser.service /etc/systemd/system/filebrowser.service
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# if config.json didn't exist, use the example server.json 
-if [ ! -f "/etc/filebrowser/config.json" ]; then
-  mv config.json /etc/filebrowser/config.json
-fi
+# set config.json
+read -p "请输入 filebrowser 端口:" port
+    [ -z "${port}" ]
+cat <<EOF >/etc/filebrowser/config.json
+{
+    "address":"127.0.0.1",
+    "database":"/opt/filebrowser/filebrowser.db",
+    "log":"/var/log/filebrowser/filebrowser.log",
+    "port":$port,
+    "root":"/home/filebrowser",
+    "username":"admin"
+}
+EOF
 
 systemctl daemon-reload
 systemctl reset-failed
