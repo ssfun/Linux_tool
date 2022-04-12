f786f3452f99363677ea771a8b08b0859ea04d78
sfun <jiegto@outlook.com>
2022-04-12 16:55:01 +0800

    Create caddy-remove.sh

---
+++ caddy/caddy-remove.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "stop caddy"
+systemctl stop caddy
+
+echo "disable caddy"
+systemctl disable caddy
+
+echo "remove caddy.service"
+rm -f /etc/systemd/system/caddy.service
+
+echo "remove caddy config file"
+rm -rf /etc/caddy
+rm -rf /usr/local/etc/caddy
+
+echo "remove caddy log file"
+rm -rf /var/log/caddy
+
+echo "remove caddy ssl file"
+rm -rf /etc/ssl/caddy
+rm -rf /home/tls
+
+echo "remove caddy www file"
+rm -rf /var/www
