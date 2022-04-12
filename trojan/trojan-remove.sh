2bbec82d3979ecfd5a5a53a2d91392bfa11be90e
sfun <jiegto@outlook.com>
2022-04-12 17:00:37 +0800

    Update trojan-remove.sh

--- trojan/trojan-remove.sh
+++ trojan/trojan-remove.sh
@@ -9,8 +9,13 @@ systemctl disable trojan-go
 echo "remove trojan-go.service"
 rm -f /etc/systemd/system/trojan-go.service
 
+echo "remove caddy"
+rm -f /usr/local/bin/trojan-go
+rm -f /usr/bin/trojan-go
+
 echo "remove trojan-go config file"
 rm -rf /etc/trojan-go
+rm -rf /usr/local/etc/trojan-go
 
 echo "remove trojan-go log file"
 rm -rf /var/log/trojan-go
