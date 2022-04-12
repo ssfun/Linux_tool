9c0d569570b25fe68fd7ca2db1c496626a233868
sfun <jiegto@outlook.com>
2022-04-12 16:59:27 +0800

    Update filebrowser-remove.sh

--- filebrowser/filebrowser-remove.sh
+++ filebrowser/filebrowser-remove.sh
@@ -9,6 +9,10 @@ systemctl disable filebrowser
 echo "remove filebrowser.service"
 rm -f /etc/systemd/system/filebrowser.service
 
+echo "remove filebrowser"
+rm -f /usr/local/bin/filebrowser
+rm -f /usr/bin/filebrowser
+
 echo "remove filebrowser config file"
 rm -rf /etc/filebrowser
 rm -rf /usr/local/etc/filebrowser
