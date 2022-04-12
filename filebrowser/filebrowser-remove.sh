42592528fcd898c00d554b2ea9d61d1123ff73df
sfun <jiegto@outlook.com>
2022-04-12 16:51:17 +0800

    Create filebrowser-remove.sh

---
+++ filebrowser/filebrowser-remove.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "stop filebrowser"
+systemctl stop filebrowser
+
+echo "disable filebrowser"
+systemctl disable filebrowser
+
+echo "remove filebrowser.service"
+rm -f /etc/systemd/system/filebrowser.service
+
+echo "remove filebrowser config file"
+rm -rf /etc/filebrowser
+rm -rf /usr/local/etc/filebrowser
+
+echo "remove trojan-go log file"
+rm -rf /var/log/filebrowser
+
+echo "remove filebrowser file"
+rm -rf /opt/filebrowser
+rm -rf /home/filebrowser
