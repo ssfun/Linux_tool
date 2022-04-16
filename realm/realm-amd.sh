11cd3ec49c0edda8ebb036f77838d42576a706b3
sfun <jiegto@outlook.com>
2022-04-16 09:50:07 +0800

    Update and rename realm/v2/realm-amd.sh to realm/realm-amd.sh

--- realm/realm-amd.sh
+++ realm/realm-amd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Getting the latest version of realm"
-latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/zephyrchien/realm/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
+latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/zhboner/realm/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
 echo "${latest_version}"
-realm_link="https://github.com/zephyrchien/realm/releases/download/${latest_version}/realm-x86_64-unknown-linux-gnu.tar.gz"
+realm_link="https://github.com/zhboner/realm/releases/download/${latest_version}/realm-x86_64-unknown-linux-gnu.tar.gz"
 
 cd `mktemp -d`
 wget -nv "${realm_link}" -O realm.tar.gz
@@ -52,6 +52,7 @@ zero_copy = true
 [[endpoints]]
 listen = "[::0]:$lport"
 remote = "$rhost:$rport"
+
 EOF
 
 
