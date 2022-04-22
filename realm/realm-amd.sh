2f29459f6119e26eca6014ca3740fadcfc785197
sfun <jiegto@outlook.com>
2022-04-22 09:45:04 +0800

    Update realm-amd.sh

--- realm/realm-amd.sh
+++ realm/realm-amd.sh
@@ -3,7 +3,7 @@
 echo "Getting the latest version of realm"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/zhboner/realm/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
 echo "${latest_version}"
-realm_link="https://github.com/zhboner/realm/releases/download/${latest_version}/realm-x86_64-unknown-linux-gnu.tar.gz"
+realm_link="https://github.abskoop.workers.dev/https://github.com/zhboner/realm/releases/download/${latest_version}/realm-x86_64-unknown-linux-gnu.tar.gz"
 
 cd `mktemp -d`
 wget -nv "${realm_link}" -O realm.tar.gz
