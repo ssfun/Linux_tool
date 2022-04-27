5c26dc00dcf859222b32c44e3e94e62bdfe90710
sfun <jiegto@outlook.com>
2022-04-27 17:50:02 +0800

    Create caddy-install.sh

---
+++ caddy/caddy-install.sh
@@ -0,0 +1,162 @@
+#!/bin/sh
+
+echo "check root user"
+check_if_running_as_root() {
+  # If you want to run as another user, please modify $EUID to be owned by this user
+  if [[ "$EUID" -ne '0' ]]; then
+    echo "error: You must run this script as root!"
+    exit 1
+  fi
+}
+
+echo "check operating system"
+identify_the_operating_system() {
+  if [[ "$(uname)" == 'Linux' ]]; then
+    case "$(uname -m)" in
+      'amd64' | 'x86_64')
+        MACHINE='amd64'
+        ;;
+      'armv8' | 'aarch64')
+        MACHINE='arm64'
+        ;;
+      *)
+        echo "error: The architecture is not supported."
+        exit 1
+        ;;
+    esac
+    if [[ ! -f '/etc/os-release' ]]; then
+      echo "error: Don't use outdated Linux distributions."
+      exit 1
+    fi
+}
+
+echo "Getting the latest version of caddy"
+latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/lxhao61/integrated-examples/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
+
+echo "${latest_version}"
+caddy_link="https://github.com/lxhao61/integrated-examples/releases/download/${latest_version}/caddy_linux_$MACHINE.tar.gz"
+
+echo "download latest version"
+cd `mktemp -d`
+wget -nv "${caddy_link}" -O caddy.tar.gz
+tar -zxvf caddy.tar.gz
+
+mv caddy /usr/local/bin/caddy && chmod +x /usr/local/bin/caddy
+
+mkdir -p "/usr/local/etc/caddy"
+mkdir -p "/var/www"
+mkdir -p "/var/www/404"
+mkdir -p "/var/log/caddy"
+
+# get 404.html
+curl -s  https://raw.githubusercontent.com/ssfun/Linux_tool/main/caddy/404/index.html  -o /var/www/404/index.html
+
+echo "set caddy.service"
+cat <<EOF >/etc/systemd/system/caddy.service
+[Unit]
+Description=Caddy
+Documentation=https://caddyserver.com/docs/
+After=network.target network-online.target
+Requires=network-online.target
+[Service]
+Type=notify
+User=root
+Group=root
+ExecStart=/usr/local/bin/caddy run --environ --config /usr/local/etc/caddy/Caddyfile
+ExecReload=/usr/local/bin/caddy reload --config /usr/local/etc/caddy/Caddyfile
+TimeoutStopSec=5s
+LimitNOFILE=1048576
+LimitNPROC=512
+PrivateTmp=true
+ProtectSystem=full
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "set Caddyfile"
+read -p "请输入需要设置的网站host:" host
+    [ -z "${host}" ]
+read -p "请输入 trojan 密码:" pswd
+    [ -z "${pswd}" ]
+read -p "请输入 filebrowser 端口:" port
+    [ -z "${port}" ]
+cat <<EOF >/usr/local/etc/caddy/Caddyfile
+{
+	order trojan before route
+	admin off
+	log { #注意：版本不小于v2.4.0才支持日志全局配置，否则各自配置。
+		level ERROR
+		output file /var/log/caddy/access.log
+	}
+	storage file_system /home/tls #自定义证书目录
+	servers :443 {
+		listener_wrappers {
+			trojan #caddy-trojan插件应用必须配置
+		}
+		protocol {
+			allow_h2c #caddy-trojan插件应用必须启用
+		}
+	}
+	auto_https off #禁用自动https
+	servers 127.0.0.1:88 { #与下边本地监听端口对应
+		protocol {
+			allow_h2c #开启h2c server支持
+		}
+	}
+}
+:80 { #http默认监听端口
+	redir https://{host}{uri} permanent #http自动跳转https,让网站看起来更真实。
+}
+:88 { #http/1.1与h2c server监听端口
+	bind 127.0.0.1 #绑定本地主机，避免本机外的机器探测到上面端口。
+	@host {
+		host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+	}
+	route @host {
+		header {
+			Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
+		}
+      reverse_proxy 127.0.0.1:$port
+	}
+}
+:443, $host { #xx.yy修改为自己的域名。注意：逗号与域名之间有一个空格。
+	tls {
+		ciphers TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
+		alpn h2 http/1.1
+	}
+	trojan {
+		user $pswd #修改为自己的密码。密码可多组，用空格隔开。
+		connect_method
+		websocket #开启WebSocket支持
+	}
+	@host {
+		host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+	}
+	route @host {
+		header {
+			Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
+		}
+		reverse_proxy 127.0.0.1:$port
+	}
+}
+http://$host:404 {
+        @host {
+                host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+        }
+        route @host {
+		file_server {
+	                root /var/www/404 #修改为自己存放的WEB文件路径
+		}
+        }
+}
+EOF
+
+echo "start caddy"
+systemctl daemon-reload
+systemctl reset-failed
+systemctl enable caddy
+systemctl start caddy
+
+echo "caddy is installed, and started."
+echo "use 'nano /usr/local/etc/caddy/Caddyfile' edit caddy Caddyfile."
