2a141fac4e9565ec777e59cdb47e6ca335a44170
sfun <jiegto@outlook.com>
2022-04-27 19:38:13 +0800

    Update realm-update.sh

--- realm/realm-update.sh
+++ realm/realm-update.sh
@@ -14,11 +14,6 @@ else
     exit 1
 fi
 
-# getting the latest version of trojan-go"
-latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
-echo -e "get the latest version of trojan-go: ${latest_version}"
-trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/${latest_version}/trojan-go-linux-${arch}.zip"
-
 echo -e "remove the old version of realm"
 systemctl stop realm
 rm -f /usr/local/bin/realm
@@ -26,7 +21,7 @@ rm -f /usr/local/bin/realm
 # getting the latest version of realm"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/zhboner/realm/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
 echo -e "get the latest version of realm: ${latest_version}"
-realm_link="https://github.abskoop.workers.dev/https://github.com/zhboner/realm/releases/download/${latest_version}/realm-x86_64-unknown-linux-gnu.tar.gz"
+realm_link="https://github.abskoop.workers.dev/https://github.com/zhboner/realm/releases/download/${latest_version}/realm-${arch}-unknown-linux-gnu.tar.gz"
 
 cd `mktemp -d`
 wget -nv "${realm_link}" -O realm.tar.gz
