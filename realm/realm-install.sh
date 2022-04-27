e71c628d1de928ad98f973dcd2a47ee601f2d2d1
sfun <jiegto@outlook.com>
2022-04-27 19:38:58 +0800

    Update and rename realm-arm.sh to realm-install.sh

--- realm/realm-install.sh
+++ realm/realm-install.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/usr/bin
 
-echo "Getting the latest version of realm"
+echo -e "check root user"
+[[ $EUID -ne 0 ]] && echo -e "Error: You must run this script as root!" && exit 1
+
+arch=$(arch)
+echo -e "get the operating system: $(arch)"
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="x86_64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="aarch64"
+else
+    echo -e "Error: The operating system is not supported."
+    exit 1
+fi
+
+# getting the latest version of realm"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/zhboner/realm/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
-echo "${latest_version}"
-realm_link="https://github.com/zhboner/realm/releases/download/${latest_version}/realm-aarch64-unknown-linux-gnu.tar.gz"
+echo -e "get the latest version of realm: ${latest_version}"
+realm_link="https://github.abskoop.workers.dev/https://github.com/zhboner/realm/releases/download/${latest_version}/realm-${arch}-unknown-linux-gnu.tar.gz"
 
 cd `mktemp -d`
 wget -nv "${realm_link}" -O realm.tar.gz
@@ -54,7 +68,6 @@ zero_copy = true
 [[endpoints]]
 listen = "$lhost:$lport"
 remote = "$rhost:$rport"
-
 EOF
 
 
@@ -63,5 +76,5 @@ systemctl reset-failed
 systemctl enable realm
 systemctl start realm
 
-echo "realm is installed, and started."
-echo "use 'nano /usr/local/etc/realm/config.toml' edit realm config."
+echo -e "realm is installed, and started."
+echo -e "use 'nano /usr/local/etc/realm/config.toml' edit realm config."
