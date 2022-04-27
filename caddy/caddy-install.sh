d2a303840811154a84c69259670851831ba05472
sfun <jiegto@outlook.com>
2022-04-27 18:02:42 +0800

    Update caddy-install.sh

--- caddy/caddy-install.sh
+++ caddy/caddy-install.sh
@@ -33,10 +33,10 @@ identify_the_operating_system() {
     exit 1
   fi
 }
+echo "$MACHINE"
 
 echo "Getting the latest version of caddy"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/lxhao61/integrated-examples/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
-
 echo "${latest_version}"
 caddy_link="https://github.abskoop.workers.dev/https://github.com/lxhao61/integrated-examples/releases/download/${latest_version}/caddy_linux_$MACHINE.tar.gz"
 
