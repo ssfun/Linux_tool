831b9cf6b0f84166450074dc2e79fe149cf60243
sfun <jiegto@outlook.com>
2022-04-27 21:25:33 +0800

    Update and rename plexdrive-amd.sh to plexdrive-install.sh

--- plexdrive/plexdrive-install.sh
+++ plexdrive/plexdrive-install.sh
@@ -1,12 +1,28 @@
-#!/bin/sh
+#!/usr/bin
 
-echo "Getting the latest version of plexdrive"
+echo -e "check root user"
+[[ $EUID -ne 0 ]] && echo -e "Error: You must run this script as root!" && exit 1
+
+arch=$(arch)
+echo -e "get operating system: $(arch)"
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="amd64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64"
+else
+    echo -e "Error: The operating system is not supported."
+    exit 1
+fi
+
+# getting the latest version of plexdrive"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/plexdrive/plexdrive/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/v//g;s/,//g;s/ //g')"
-echo "${latest_version}"
-plexdrive_link="https://github.com/plexdrive/plexdrive/releases/download/${latest_version}/plexdrive-linux-amd64"
+echo "get the latest version of plexdrive: ${latest_version}"
+plexdrive_link="https://github.com/plexdrive/plexdrive/releases/download/${latest_version}/plexdrive-linux-${arch}"
 
 mkdir -p "/home/gsuite"
 mkdir -p "/home/.plexdrive"
+
+echo -e "installing the latest version of plexdrive"
 
 cd `mktemp -d`
 wget -nv "${plexdrive_link}" -O plexdrive
