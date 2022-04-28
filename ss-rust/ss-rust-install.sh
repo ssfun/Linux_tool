bfbeff93493368cd6703b3eac53b3e4a69864f56
sfun <jiegto@outlook.com>
2022-04-28 14:56:50 +0800

    Update and rename ss-rust-amd.sh to ss-rust-install.sh

--- ss-rust/ss-rust-install.sh
+++ ss-rust/ss-rust-install.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/usr/bin
 
-echo "Getting the latest version of shadowsocks-rust"
+echo -e "check root user"
+[[ $EUID -ne 0 ]] && echo -e "Error: You must run this script as root!" && exit 1
+
+arch=$(arch)
+echo -e "get operating system: $(arch)"
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="x86_64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="aarch64"
+else
+    echo -e "Error: The operating system is not supported."
+    exit 1
+fi
+
+# getting the latest version of shadowsocks-rust"
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
-echo "${latest_version}"
-ss_link="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${latest_version}/shadowsocks-${latest_version}.x86_64-unknown-linux-gnu.tar.xz"
+echo -e "get the latest version of ss-rust: ${latest_version}"
+ss_link="https://github.com/shadowsocks/shadowsocks-rust/releases/download/${latest_version}/shadowsocks-${latest_version}.${arch}-unknown-linux-gnu.tar.xz"
 
+echo -e "installing the latest version of plexdrive"
 cd `mktemp -d`
 wget -nv "${ss_link}" -O ss.tar.xz
 xz -d ss.tar.xz && tar -xf ss.tar
