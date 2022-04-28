c3f126b4bfb75a53d2a3f118b5e9220ee572e88e
sfun <jiegto@outlook.com>
2022-04-28 15:03:39 +0800

    Update and rename oci-help-amd.sh to oci-help-install.sh

---
+++ oci-help/oci-help-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin
+
+echo -e "check root user"
+[[ $EUID -ne 0 ]] && echo -e "Error: You must run this script as root!" && exit 1
+
+arch=$(arch)
+echo -e "get operating system: $(arch)"
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="amd64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64"
+else
+    echo -e "Error: The operating system is not supported."
+    exit 1
+fi
+
+# getting the latest version of oci-help"
+latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/lemoex/oci-help/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
+echo -e "get the latest version of oci-help: ${latest_version}"
+oci_link="https://github.com/lemoex/oci-help/releases/download/${latest_version}/oci-help-linux-${arch}-${latest_version}.zip"
+
+echo -e "installing the latest version of oci-help"
+mkdir -p "/root/oci"
+wget -nv "${oci_link}" -O oci.zip
+unzip oci.zip -d /root/oci && rm -f oci.zip
+chmod +x /root/oci/oci-help
+
+echo "oci-help is installed. use 'apt install screen' to install screen, then 'screen -S oci-help' "
