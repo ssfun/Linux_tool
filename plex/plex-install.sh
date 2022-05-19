ad8737a39ab6cc0271df8c2e4972419b4b8fae30
sfun <jiegto@outlook.com>
2022-05-19 18:18:38 +0800

    Update plex-install.sh

--- plex/plex-install.sh
+++ plex/plex-install.sh
@@ -19,7 +19,7 @@ read -p "若需要下载 plexpass 版本请输入 Plex-Token :" token
     [ -z "${token}" ]
 
 # getting the latest version of plex media server"
-latest_version="$(curl -s 'https://plex.tv/api/downloads/5.json?channel=plexpass'  \-X 'GET' \-H 'X-Plex-Token: $token' | grep -o '"version":"[^"]*' | grep -o '[^"]*$' | head -n 1)"
+latest_version="$(curl -s 'https://plex.tv/api/downloads/5.json?channel=plexpass'  \-X 'GET' \-H 'X-Plex-Token: ${token}' | grep -o '"version":"[^"]*' | grep -o '[^"]*$' | head -n 1)"
 echo -e "get the latest version of plex media server: ${latest_version}"
 plex_link="https://downloads.plex.tv/plex-media-server-new/${latest_version}/debian/plexmediaserver_${latest_version}_${arch}.deb"
 
