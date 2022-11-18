40376e76c074c38f25db3014100a7399f4b5de8a
sfun <jiegto@outlook.com>
2022-11-18 14:08:48 +0800

    Update caddy-plex.sh

--- caddy/caddy-plex.sh
+++ caddy/caddy-plex.sh
@@ -17,7 +17,7 @@ fi
 # getting the latest version of caddy
 latest_version="$(wget -qO- -t1 -T2 "https://api.github.com/repos/lxhao61/integrated-examples/releases" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')"
 echo -e "get the latest version of caddy: ${latest_version}"
-caddy_link="https://github.com/lxhao61/integrated-examples/releases/download/${latest_version}/caddy_linux_${arch}.tar.gz"
+caddy_link="https://github.com/lxhao61/integrated-examples/releases/download/${latest_version}/caddy-linux-${arch}.tar.gz"
 
 echo -e "installing the latest version of caddy"
 cd `mktemp -d`
@@ -70,93 +70,103 @@ read -p "请输入需要设置的Plex网站:" plex
     [ -z "${plex}" ]
 cat <<EOF >/usr/local/etc/caddy/Caddyfile
 {
-	order trojan before route
-	admin off
-	log { #注意：版本不小于v2.4.0才支持日志全局配置，否则各自配置。
-		level ERROR
-		output file /var/log/caddy/access.log
-	}
-	storage file_system /home/tls #自定义证书目录
-	servers :443 {
-		listener_wrappers {
-			trojan #caddy-trojan插件应用必须配置
-		}
-		protocol {
-			allow_h2c #caddy-trojan插件应用必须启用
-		}
-	}
-	auto_https off #禁用自动https
-	servers 127.0.0.1:88 { #与下边本地监听端口对应
-		protocol {
-			allow_h2c #开启h2c server支持
-		}
-	}
+        order trojan before route
+        order forward_proxy before trojan
+        order reverse_proxy before forward_proxy
+        admin off
+        log {
+                output file /var/log/caddy/error.log
+                level ERROR
+        }       #版本不小于v2.4.0才支持日志全局配置，否则各自配置。
+        storage file_system {
+                root /home/tls #存放TLS证书的基本路径
+        }
+
+        cert_issuer acme #acme表示从Let's Encrypt申请TLS证书，zerossl表示从ZeroSSL申请TLS证书。必须acme与zerossl二选一（固定TLS证书的目录便于引用）。注意：版本不小于v2.4.1才支持。
+        email $mail #电子邮件地址。选配，推荐。
+
+        servers 127.0.0.1:88 {
+                #与下边本地监听端口对应
+                listener_wrappers {
+                        proxy_protocol #开启PROXY protocol接收
+                }
+                protocols h1 h2c #开启HTTP/1.1 server与H2C server支持
+        }
+        servers :443 {
+                #与下边本地监听端口对应
+                listener_wrappers {
+                        trojan #caddy-trojan插件应用必须配置
+                }
+                protocols h1 h2 h3 #开启HTTP/3 server支持（默认，此条参数可以省略不写。）。若采用HAProxy SNI分流（目前不支持UDP转发），推荐不开启。
+        }
+        trojan {
+                caddy
+                no_proxy
+                users $pswd #修改为自己的密码。密码可多组，用空格隔开。
+        }
 }
 
-:80 { #http默认监听端口
-	redir https://{host}{uri} permanent #http自动跳转https,让网站看起来更真实。
+:80 {
+        #HTTP默认监听端口
+        redir https://{host}{uri} permanent #HTTP自动跳转HTTPS，让网站看起来更真实。
 }
 
-:88 { #http/1.1与h2c server监听端口
-	bind 127.0.0.1 #绑定本地主机，避免本机外的机器探测到上面端口。
-	@host {
-		host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
-	}
-	route @host {
-		header {
-			Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
-		}
-        reverse_proxy 127.0.0.1:$port
-	}
+:88 {
+        #HTTP/1.1 server及H2C server监听端口
+        bind 127.0.0.1 #绑定本地主机，避免本机外的机器探测到上面端口。
+        @host {
+                host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+        }
+        route @host {
+                header {
+                        Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
+                }
+                reverse_proxy 127.0.0.1:40333
+        }
 }
 
-:443, $host, $plex { #xx.yy修改为自己的域名。注意：逗号与域名之间有一个空格。
-	tls {
-		ciphers TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
-		alpn h2 http/1.1
-	}
-
-	trojan {
-		user $pswd #修改为自己的密码。密码可多组，用空格隔开。
-		connect_method
-		websocket #开启WebSocket支持
-	}
-
-	@host {
-		host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
-	}
-	route @host {
-		header {
-			Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
-		}
-		reverse_proxy 127.0.0.1:$port
-	}
-
-	@plex {
-		host $plex #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
-	}
-	route @plex {
-		header {
-			Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
+:443, $host:443, $plex:443 {
+        #HTTPS server监听端口。注意：逗号与域名（或含端口）之间有一个空格。
+        tls {
+                ciphers TLS_AES_256_GCM_SHA384 TLS_AES_128_GCM_SHA256 TLS_CHACHA20_POLY1305_SHA256 TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
+                curves x25519 secp521r1 secp384r1 secp256r1
+        }
+        trojan {
+                connect_method
+                websocket #开启WebSocket支持
+        }       #此部分配置为caddy-trojan插件的WebSocket应用，若删除就仅支持Trojan应用。
+        @host {
+                host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+        }
+        route @host {
+                header {
+                        Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
+                }
+                reverse_proxy 127.0.0.1:$port
+        }
+        @plex {
+                host $plex #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
+        }
+        route @plex {
+                header {
+                        Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" #启用HSTS
                         X-Content-Type-Options nosniff
                         X-Frame-Options DENY
                         Referrer-Policy no-referrer-when-downgrade
                         X-XSS-Protection 1
-		}
-		reverse_proxy 127.0.0.1:32400
+                }
+                reverse_proxy 127.0.0.1:32400
                 encode gzip
-	}
-
+        }
 }
-
 http://$host:404 {
         @host {
                 host $host #限定域名访问（禁止以ip方式访问网站），修改为自己的域名。
         }
         route @host {
-		file_server {
-	                root /var/www/404 #修改为自己存放的WEB文件路径
-		}
+                file_server {
+                        root /var/www/404 #修改为自己存放的WEB文件路径
+                }
         }
 }
 EOF
